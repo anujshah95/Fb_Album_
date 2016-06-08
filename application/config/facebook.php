4b41ec74fc6665d9759408bc172ff0b71d1cfb2e
Anuj Shah <anuj.shah95@gmail.com>
2016-06-08 16:05:17 +0530

    get and display all information of logged in user

---
+++ application/config/facebook.php
@@ -0,0 +1,7 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+
+$config['appId']   = '1041043902651043';
+$config['secret']  = '3a0b300ff7da9715d02ab66728a4ec86';
+
+?>
